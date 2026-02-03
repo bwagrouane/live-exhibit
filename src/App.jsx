6865a9370f10b97d5f4d1b97756958581428524d
bwagrouane <bilaluzamaki@gmail.com>
2026-02-02 23:13:58 -0500

    add title, card overlay, trying to make it look good

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@
 
 function App() {
   return (
-      <div className={"backdrop-blur-md h-screen  w-3/4 bg-pink-300/15 border border-white/20  mx-auto"} >
-
+      <div className={"backdrop-blur-md h-screen bg-pink-300/15  w-3/4  border border-white/20  mx-auto"} >
+          <div className={" mt-6 w-1/3 h-1/6 shadow-2xl/10  rounded-2xl mx-auto flex justify-center items-center border border-white/20"} >
+              <h1 className={"text-6xl text-center font-mono text-gray-200 "}>Live Exhibit</h1>
+          </div>
       </div>
   )
 }
