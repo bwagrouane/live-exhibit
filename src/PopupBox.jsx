d3708960a3f97f1bdb9b420e0a368bb3cd18494c
bwagrouane <bilaluzamaki@gmail.com>
2026-02-10 00:12:50 -0500

    nonfunctional x button + cosmetic changes to popupbox

--- src/PopupBox.jsx
+++ src/PopupBox.jsx
@@ -9,8 +9,8 @@ function PopupBox({isOpen}) {
 
     return (
 
-        <div className={"fixed w-7/8 h-13/16 lg:h-3/4 bg-pink-300/45 border-white/20 inset-0 m-auto rounded-4xl z-50 "}>
-
+        <div className={"fixed w-10/11 shadow-2xl/10 h-13/16 lg:h-3/4 bg-pink-300/15  border border-white/20 inset-0 m-auto rounded-4xl z-50 "}>
+            <button className={" w-10 h-10 absolute top-4 right-4 text-pink-500  bg-pink-300/45 border border-white/20 rounded-full "}>X</button>
         </div>
 
     )
