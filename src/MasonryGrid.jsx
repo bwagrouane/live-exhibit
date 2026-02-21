80cc09c0f8a395fbf0540e400a818b168904edd7
bwagrouane <bilaluzamaki@gmail.com>
2026-02-21 18:01:24 -0500

    Added image fetch from database

--- src/MasonryGrid.jsx
+++ src/MasonryGrid.jsx
@@ -1,6 +1,9 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ImgPopupBox from "./ImgPopupBox.jsx";
 import ImgDisplay from "./ImgDisplay.jsx";
+import supabase from "./supabaseclient.js";
+
+
 function MasonryGrid() {
 
    const [isOpen,setIsOpen] = useState(false);
@@ -16,6 +19,36 @@ function MasonryGrid() {
        setIsOpen(false);
        setSelectedImg(null);
    }
+
+
+   useEffect(() => {
+
+       async function fetchData(){
+       const { data, error } = await supabase
+           .from('Images')
+           .select()
+           .order('id', { ascending: false })
+           .limit(5)
+
+
+            if(error){
+                console.log(error)
+            }
+            else console.log(data);
+
+
+       }
+
+       fetchData();
+   }, []);
+
+
+
+
+
+
+
+
 
     return (
         <div  className={" bg-pink-300/30 w-6/7 min-h-160 lg:min-h-170  columns-2 lg:columns-5 gap-1 mt-4 mx-auto"}>
